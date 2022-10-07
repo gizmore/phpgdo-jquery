c09312420020559c98b14a9f8c280a81be518b89
gizmore <gizmore@wechall.net>
2022-10-07 07:01:58 +0200

    '7.0.1-r1649 German Lang Pack - I'

---
+++ lang/jquery_de.php
@@ -0,0 +1,4 @@
+<?php
+namespace GDO\JQuery\lang;
+return [
+'cfg_div_extensions' => 'jQuery-Erweiterungen','cfg_jquery_modal' => 'jQuery-Modal-Plugin verwenden?','cfg_jquery_color' => 'jQuery Color Plugin verwenden?','cfg_jquery_gdo_effects' => 'jQuery GDOv7-Effekte verwenden?',];
