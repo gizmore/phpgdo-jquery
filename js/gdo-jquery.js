6618015cb6be2a1131bf73b3010599acc1892050
gizmore <gizmore@wechall.net>
2022-12-15 08:45:16 +0100

    '7.0.1-r1712 bootstrap5theme'

--- js/gdo-jquery.js
+++ js/gdo-jquery.js
@@ -2,11 +2,12 @@
 $(function(){
 
 	window.GDO.error = function(html, title) {
-		var dialog = $('<div class="gdo-modal modal"><h5>' + title + '</h5><div class="gdt-divider"></div><pre>' + html + '<pre></div>');
+		var dialog = $('<div class="gdo-modal modal"><i class="icon-remove">X</i><h5>' + title + '</h5><div class="gdt-divider"></div><pre>' + html + '<pre></div>');
 		$('body').append(dialog);
 		$('body').append('<div class="blocker"></div>');
 		$('.gdo-modal').modal({
 			closeClass: 'icon-remove',
+			blockerClass: 'blocker',
 		});
 		var defer = $.Deferred();
 		dialog.on($.modal.CLOSE, function() {
