7da809c6c32ecaac4a1856d40d467a49b147063c
gizmore <gizmore@wechall.net>
2026-09-18 04:08:10 +0200

    'more korean'

---
+++ lang/jquery_ko.php
@@ -0,0 +1,9 @@
+<?php
+namespace GDO\JQuery\lang;
+
+return [
+	'cfg_div_extensions' => 'jQuery 확장',
+	'cfg_jquery_modal' => 'jQuery 모달 플러그인을 사용하시겠습니까?',
+	'cfg_jquery_color' => 'jQuery 색상 플러그인을 사용하시겠습니까?',
+	'cfg_jquery_gdo_effects' => 'jQuery GDOv7 효과를 사용하시겠습니까?',
+];
